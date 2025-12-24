61941338600dc24cbaaefb071bce25abb85b7be8
minsukim9900 <mmsskksw9900@gmail.com>
2025-12-24 13:47:07 +0900

    feat: 도메인 변경으로 SecurityConfig.class 추가

--- src/main/java/com/ssafy/yogiattacku/security/config/SecurityConfig.java
+++ src/main/java/com/ssafy/yogiattacku/security/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://www.yogiattacku.shop", "https://api.yogiattacku.shop"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://www.yogiattacku.shop", "https://api.yogiattacku.shop", "https://local.yogiattacku.shop:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Cookie", "X-Requested-With", "Accept"));
         config.setExposedHeaders(List.of("Authorization", "Set-Cookie"));
